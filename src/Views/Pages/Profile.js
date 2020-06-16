896b01d9e9f0aee523a431832b4ec48c8bca41aa
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-06-16 09:39:07 -0400

    Cambios viewProfile y css profile

--- src/Views/Pages/Profile.js
+++ src/Views/Pages/Profile.js
@@ -3,40 +3,77 @@ import { viewFooter } from '../Components/Footer.js';
 
 export const viewProfile = () => {
   const viewProfileContainer = document.getElementById('page_container');
+  const user = firebase.auth().currentUser;
   viewProfileContainer.innerHTML = `
-    <div id="containerProfile">
-      <div id="nameAndImgProfile">
-        <h3></h3>
-        <img scr="<img>
+    <div id="containerPageProfile">
+      <div id="containerProfile">
+        <div id="containerDescriptionUsername">
+            <p id="nameProfile">${user.displayName}</p>
+            <label for="description"><b>Descripción:</b></label>
+            <span id="profileDescription"></span>
+            <div id="toAdd"></div>
+            <label for="mail"><b>Mail de contacto:</b></label>
+            <span class="profileMail">${user.email}</span>
+            <label for="age"><b>Edad:</b></label>
+            <span id="profileAge"></span>
+            <div id="toAdd1"></div>
+            <label for="location"><b>De donde eres:</b></label>
+            <span id="profileLocation"></span>
+            <div id="toAdd2"></div>
+        </div>
+        <div id="containerImgAndButton">
+          <img id="imgProfile" scr="${user.photoURL}">
+          <div class="addButton">
+            <button id="editProfile">Editar perfil</button>
+          </div>
+        </div>
       </div>
-        <label for="description"><b>Descripción:</b></label>
-        <span class="profileDescription"></span>
-        <label for="mail"><b>Mail de contacto:</b></label>
-        <span class="profileMail"></span>
-        <label for="age"><b>Edad:</b></label>
-        <span class="profileAge"></span>
-        <label for="location"><b>De donde eres:</b></label>
-        <span class="profileLocation"></span>
-        <button>editar perfil</button>
-    </div>
-  `;
+    </div>`;
   viewHeaderFeed();
   viewFooter();
 
-  /* const user = firebase.auth().currentUser;
-  let name = {};
-  let email = {};
-  let photoUrl = {};
-  let uid = {};
-  let emailVerified = {};
+  // ----------------------------FUNCION EDITAR POST---------------------------->
+  const editProfileUser = (uid) => {
+    const profileDescription = document.querySelector('#profileDescription');
+    // profileDescription.style.display = 'none';
+    const profileAge = document.querySelector('#profileAge');
+    // profileAge.style.display = 'none';
+    const profileLocation = document.querySelector('profileLocation');
+    // profileLocation.style.display = 'none';
 
-  if (user != null) {
-    name = user.displayName;
-    email = user.email;
-    photoUrl = user.photoURL;
-    emailVerified = user.emailVerified;
-    uid = user.uid;
-  } */
+    const toAdd = document.querySelector('#toAdd');
+    toAdd.innerHTML = `
+      <textarea id="editProfileUserr"></textarea>`;
 
+    const addButton = document.querySelector('.addButton');
+    addButton.innerHTML = `
+    <button id="saveProfile">Guardar</button>`;
+
+    addButton.onclick = () => {
+      const profileRef = firebase.firestore().collection('InformacionDePerfil').doc(uid);
+      return profileRef.update({
+        description: profileDescription.value,
+        age: profileAge.value,
+        location: profileLocation.value,
+      })
+        .then(() => {
+          console.log('Document successfully updated!');
+          toAdd.style.display = 'hidden';
+          addButton.style.display = 'hidden';
+        })
+        .catch((error) => {
+          console.error('Error updating document: ', error);
+        });
+    };
+  };
+
+  // ----------------------------BOTÓN EDDITAR POST---------------------------->
+  const editProfile = document.querySelector('#editProfile');
+  editProfile.addEventListener('click', (doc) => {
+    const result = window.confirm('¿Quieres editar tu perfil?');
+    if (result) {
+      editProfileUser(doc.id);
+    }
+  });
   return viewProfileContainer;
 };
